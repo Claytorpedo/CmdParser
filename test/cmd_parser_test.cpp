2830346c8db5fd7e607341cf4de4041e09c0e182
Claytorpedo <claytorpedo@gmail.com>
2020-06-18 21:28:06 -0700

    Use explicit error handling.

--- test/cmd_parser_test.cpp
+++ test/cmd_parser_test.cpp
@@ -1,21 +1,24 @@
 #include "definitions.hpp"
 
+namespace {
+auto ErrorSink(cmd::Error) { return cmd::ErrorResult::Continue; }
+}
+
+
 SCENARIO("There are no arguments.") {
-	std::ostringstream errorSink;
-	cmd::CmdParser cmdParser(errorSink);
+	cmd::CmdParser cmdParser;
 	GIVEN("There is no input.") {
 		const char* const args[] = { "" };
-		CHECK(cmdParser.parse(1, args));
+		CHECK(cmdParser.parse(1, args, ErrorSink));
 	}
 	GIVEN("There is unexpected input.") {
 		const char* const args[] = { "", "-a" };
-		CHECK_FALSE(cmdParser.parse(2, args));
+		CHECK_FALSE(cmdParser.parse(2, args, ErrorSink));
 	}
 }
 
 SCENARIO("Taking flag arguments.") {
-	std::ostringstream errorSink;
-	cmd::CmdParser cmdParser(errorSink);
+	cmd::CmdParser cmdParser;
 	bool flagA;
 	bool flagB;
 	bool flagNotC;
@@ -24,56 +27,56 @@ SCENARIO("Taking flag arguments.") {
 	cmdParser.pushFlag(flagNotC, 'c', "flagC", true, "Flag not C");
 	GIVEN("Flag A is set.") {
 		const char* const args[] = { "", "-a" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK(flagA);
 		CHECK_FALSE(flagB);
 		CHECK(flagNotC);
 	}
 	GIVEN("Flag B is set.") {
 		const char* const args[] = { "", "-b" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK_FALSE(flagA);
 		CHECK(flagB);
 		CHECK(flagNotC);
 	}
 	GIVEN("Flag C is set.") {
 		const char* const args[] = { "", "-c" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK_FALSE(flagA);
 		CHECK_FALSE(flagB);
 		CHECK_FALSE(flagNotC);
 	}
 	GIVEN("All flags are set separately.") {
 		const char* const args[] = { "", "-a", "-b", "-c" };
-		CHECK(cmdParser.parse(4, args));
+		CHECK(cmdParser.parse(4, args, ErrorSink));
 		CHECK(flagA);
 		CHECK(flagB);
 		CHECK_FALSE(flagNotC);
 	}
 	GIVEN("All flags are set together.") {
 		const char* const args[] = { "", "-abc" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK(flagA);
 		CHECK(flagB);
 		CHECK_FALSE(flagNotC);
 	}
 	GIVEN("Two flags are set together, one separate.") {
 		const char* const args[] = { "", "-ac", "-b" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(flagA);
 		CHECK(flagB);
 		CHECK_FALSE(flagNotC);
 	}
 	GIVEN("Flag A is set using its word key, b and c by group.") {
 		const char* const args[] = { "", "--flagA", "-bc" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(flagA);
 		CHECK(flagB);
 		CHECK_FALSE(flagNotC);
 	}
 	GIVEN("All flags are set with word keys.") {
 		const char* const args[] = { "", "--flagA", "--flagB", "--flagC" };
-		CHECK(cmdParser.parse(4, args));
+		CHECK(cmdParser.parse(4, args, ErrorSink));
 		CHECK(flagA);
 		CHECK(flagB);
 		CHECK_FALSE(flagNotC);
@@ -81,7 +84,7 @@ SCENARIO("Taking flag arguments.") {
 	GIVEN("All flags are set twice.") {
 		const char* const args[] = { "", "-abc", "--flagA", "--flagB", "--flagC" };
 		THEN("There is no difference from setting them once.") {
-			CHECK(cmdParser.parse(5, args));
+			CHECK(cmdParser.parse(5, args, ErrorSink));
 			CHECK(flagA);
 			CHECK(flagB);
 			CHECK_FALSE(flagNotC);
@@ -90,7 +93,7 @@ SCENARIO("Taking flag arguments.") {
 
 	GIVEN("All flags are set incorrectly.") {
 		const char* const args[] = { "", "flagA", "-flagB", "--c" };
-		CHECK_FALSE(cmdParser.parse(4, args));
+		CHECK_FALSE(cmdParser.parse(4, args, ErrorSink));
 		CHECK_FALSE(flagA);
 		CHECK_FALSE(flagB);
 		CHECK(flagNotC);
@@ -104,27 +107,26 @@ SCENARIO("Taking bool arguments.") {
 	cmdParser.push(boolB, 'b', "acceptB", false, "Bool B");
 	GIVEN("Bool A is set.") {
 		const char* const args[] = { "", "-a=true" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK(boolA);
 		CHECK_FALSE(boolB);
 	}
 	GIVEN("Bool B is set.") {
 		const char* const args[] = { "", "--acceptB=yes" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK_FALSE(boolA);
 		CHECK(boolB);
 	}
 	GIVEN("Bool A is set to false, and b to true.") {
 		const char* const args[] = { "", "--boolA=false", "-b=y" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK_FALSE(boolA);
 		CHECK(boolB);
 	}
 }
 
 SCENARIO("Taking arithmetic arguments.") {
-	std::ostringstream errorSink;
-	cmd::CmdParser cmdParser(errorSink);
+	cmd::CmdParser cmdParser;
 	int32_t intArg;
 	uint32_t unsignedIntArg;
 	char charArg;
@@ -141,7 +143,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	cmdParser.push(int16, 't');
 	GIVEN("An integer.") {
 		const char* const args[] = { "", "-i", "-10" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(intArg == -10);
 		CHECK(unsignedIntArg == 1);
 		CHECK(charArg == 'a');
@@ -152,7 +154,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	}
 	GIVEN("An unsigned integer.") {
 		const char* const args[] = { "", "-u", "4000000000" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(intArg == 0);
 		CHECK(unsignedIntArg == 4'000'000'000);
 		CHECK(charArg == 'a');
@@ -163,7 +165,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	}
 	GIVEN("A char.") {
 		const char* const args[] = { "", "-c", "G" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(intArg == 0);
 		CHECK(unsignedIntArg == 1);
 		CHECK(charArg == 'G');
@@ -174,7 +176,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	}
 	GIVEN("A float.") {
 		const char* const args[] = { "", "-f", "14.897" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(intArg == 0);
 		CHECK(unsignedIntArg == 1);
 		CHECK(charArg == 'a');
@@ -185,7 +187,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	}
 	GIVEN("An int8_t.") {
 		const char* const args[] = { "", "-s", "-40" };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(intArg == 0);
 		CHECK(unsignedIntArg == 1);
 		CHECK(charArg == 'a');
@@ -196,7 +198,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	}
 	GIVEN("A uint8_t.") {
 		const char* const args[] = { "", "--small-uint=1" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK(intArg == 0);
 		CHECK(unsignedIntArg == 1);
 		CHECK(charArg == 'a');
@@ -207,7 +209,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	}
 	GIVEN("An int16_t.") {
 		const char* const args[] = { "", "-t=-10000" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK(intArg == 0);
 		CHECK(unsignedIntArg == 1);
 		CHECK(charArg == 'a');
@@ -218,7 +220,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	}
 	GIVEN("All are set separately.") {
 		const char* const args[] = { "", "--int", "88", "--uint=12", "-c=s", "--float=-98.123", "-s=10", "--small-uint", "40", "-t=100" };
-		CHECK(cmdParser.parse(10, args));
+		CHECK(cmdParser.parse(10, args, ErrorSink));
 		CHECK(intArg == 88);
 		CHECK(unsignedIntArg == 12);
 		CHECK(charArg == 's');
@@ -230,7 +232,7 @@ SCENARIO("Taking arithmetic arguments.") {
 	GIVEN("A char when we expect an integer.") {
 		const char* const args[] = { "", "--small-int", "a" };
 		THEN("Parsing should fail.") {
-			CHECK_FALSE(cmdParser.parse(3, args));
+			CHECK_FALSE(cmdParser.parse(3, args, ErrorSink));
 			CHECK(smallIntArg == 0);
 		}
 	}
@@ -249,49 +251,49 @@ SCENARIO("Giving overflow values for integer arguments.") {
 	GIVEN("A value larger than an int32_t can hold.") {
 		const char* const args[] = { "", "-i", "10000000000000" };
 		THEN("The value should default to the max it can hold.") {
-			CHECK(cmdParser.parse(3, args));
+			CHECK(cmdParser.parse(3, args, ErrorSink));
 			CHECK(intArg == std::numeric_limits<int32_t>::max());
 		}
 	}
 	GIVEN("A value smaller than an int32_t can hold.") {
 		const char* const args[] = { "", "-i", "-10000000000000" };
 		THEN("The value should default to the min it can hold.") {
-			CHECK(cmdParser.parse(3, args));
+			CHECK(cmdParser.parse(3, args, ErrorSink));
 			CHECK(intArg == std::numeric_limits<int32_t>::min());
 		}
 	}
 	GIVEN("A value larger than an int8_t can hold.") {
 		const char* const args[] = { "", "-s", "200" };
 		THEN("The value should default to the max it can hold.") {
-			CHECK(cmdParser.parse(3, args));
+			CHECK(cmdParser.parse(3, args, ErrorSink));
 			CHECK(smallIntArg == std::numeric_limits<int8_t>::max());
 		}
 	}
 	GIVEN("A value smaller than an int8_t can hold.") {
 		const char* const args[] = { "", "-s", "-200" };
 		THEN("The value should default to the min it can hold.") {
-			CHECK(cmdParser.parse(3, args));
+			CHECK(cmdParser.parse(3, args, ErrorSink));
 			CHECK(smallIntArg == std::numeric_limits<int8_t>::min());
 		}
 	}
 	GIVEN("A value larger than a unt8_t can hold.") {
 		const char* const args[] = { "", "-u", "256" };
 		THEN("The value should default to the max it can hold.") {
-			CHECK(cmdParser.parse(3, args));
+			CHECK(cmdParser.parse(3, args, ErrorSink));
 			CHECK(smallUintArg == std::numeric_limits<uint8_t>::max());
 		}
 	}
 	GIVEN("A value larger than an int16_t can hold.") {
 		const char* const args[] = { "", "-t", "33000" };
 		THEN("The value should default to the max it can hold.") {
-			CHECK(cmdParser.parse(3, args));
+			CHECK(cmdParser.parse(3, args, ErrorSink));
 			CHECK(int16 == std::numeric_limits<int16_t>::max());
 		}
 	}
 	GIVEN("A value smaller than an int16_t can hold.") {
 		const char* const args[] = { "", "-t", "-33000" };
 		THEN("The value should default to the min it can hold.") {
-			CHECK(cmdParser.parse(3, args));
+			CHECK(cmdParser.parse(3, args, ErrorSink));
 			CHECK(int16 == std::numeric_limits<int16_t>::min());
 		}
 	}
@@ -306,19 +308,19 @@ SCENARIO("Taking string arguments") {
 	GIVEN("A string.") {
 		const std::string input = "this is a test string";
 		const char* const args[] = { "", "-s", input.c_str() };
-		CHECK(cmdParser.parse(3, args));
+		CHECK(cmdParser.parse(3, args, ErrorSink));
 		CHECK(strArg == input);
 		CHECK(strViewArg == "default");
 	}
 	GIVEN("A string view.") {
 		const char* const args[] = { "", "-v=test string view" };
-		CHECK(cmdParser.parse(2, args));
+		CHECK(cmdParser.parse(2, args, ErrorSink));
 		CHECK(strArg.empty());
 		CHECK(strViewArg == "test string view");
 	}
 	GIVEN("They are both set.") {
 		const char* const args[] = { "", "--str=Test string", "--view", "Test string_view" };
-		CHECK(cmdParser.parse(4, args));
+		CHECK(cmdParser.parse(4, args, ErrorSink));
 		CHECK(strArg == "Test string");
 		CHECK(strViewArg == "Test string_view");
 	}
@@ -340,7 +342,7 @@ SCENARIO("Taking a mixture of argument types.") {
 	cmdParser.push(viewArg, 'v');
 	GIVEN("No input.") {
 		const char* const args[] = { "" };
-		CHECK(cmdParser.parse(1, args));
+		CHECK(cmdParser.parse(1, args, ErrorSink));
 		THEN("All values are their defaults.") {
 			CHECK_FALSE(flag);
 			CHECK_FALSE(boolArg);
@@ -352,7 +354,7 @@ SCENARIO("Taking a mixture of argument types.") {
 	}
 	GIVEN("They are all set in a random order.") {
 		const char* const args[] = { "", "-i", "100", "-v=StringView", "--float", "-0.17", "-f", "-s", "987654321", "--bool=false" };
-		CHECK(cmdParser.parse(10, args));
+		CHECK(cmdParser.parse(10, args, ErrorSink));
 		THEN("They are all set correctly.") {
 			CHECK(flag);
 			CHECK_FALSE(boolArg);
@@ -364,7 +366,7 @@ SCENARIO("Taking a mixture of argument types.") {
 	}
 	GIVEN("A mixture are set.") {
 		const char* const args[] = { "", "-f", "--int=-9", "--float", "8", "--bool=t" };
-		CHECK(cmdParser.parse(6, args));
+		CHECK(cmdParser.parse(6, args, ErrorSink));
 		CHECK(flag);
 		CHECK(boolArg);
 		CHECK(intArg == -9);
